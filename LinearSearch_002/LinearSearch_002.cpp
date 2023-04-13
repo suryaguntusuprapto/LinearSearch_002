b7460808094162ed84858a15852f287d0b597e13
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-04-13 10:00:13 +0700

    inputan

--- LinearSearch_002/LinearSearch_002.cpp
+++ LinearSearch_002/LinearSearch_002.cpp
@@ -4,3 +4,12 @@ using namespace std;
 int arr[20]; //array to be searched
 int n;
 int i;
+
+void input() {
+
+    while (true) {
+        cout << "Enter the number of element in the array : ";
+        cin >> n;
+    }
+}
+
